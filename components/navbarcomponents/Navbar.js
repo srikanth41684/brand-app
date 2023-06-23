9cada96db25a550ba3aaf2a9174d84d96d32cefe
Usnagiri Srikanth <UsnagiriS@jmf.com>
2023-06-23 17:52:36 +0530

    nav links

--- components/navbarcomponents/Navbar.js
+++ components/navbarcomponents/Navbar.js
@@ -2,32 +2,35 @@ import Image from "next/image";
 import logo from "/public/logo.png";
 import SearchInput from "./SearchInput";
 import Link from "next/link";
+import wishImage from "/public/wishlist-icon.png";
+import cartImage from "/public/cart-icon.png";
+import profileImage from "/public/profile-icon.png";
 
 const navList = [
   {
     id: 1,
     title: "Wish List",
-    src: "",
+    src: wishImage,
     href: "/wishlist",
   },
   {
     id: 2,
     title: "My Cart",
-    src: "",
+    src: cartImage,
     href: "/cart",
   },
   {
     id: 1,
     title: "Profile",
-    src: "",
+    src: profileImage,
     href: "/profile",
   },
 ];
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="flex items-center">
+    <div className="">
+      <div className="flex items-center justify-between px-12 max-w-[1680px] m-auto">
         <div className="flex items-center">
           <Image src={logo} width={50} height={50} alt="logo" />
           <div className="text-3xl text-sky-300 font-bold pl-2">Barand</div>
@@ -42,7 +45,7 @@ const Navbar = () => {
                 href={item.href}
                 className="flex flex-col px-1 items-center"
               >
-                <Image src={logo} width={50} height={50} alt="logo" />
+                <Image src={item.src} width={23} height={23} alt="logo" />
                 <div>{item.title}</div>
               </Link>
             );

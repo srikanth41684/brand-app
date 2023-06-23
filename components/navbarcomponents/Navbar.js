2eb832cf5b43b113e226498d63a3ccd4e89f5065
Usnagiri Srikanth <UsnagiriS@jmf.com>
2023-06-23 15:50:58 +0530

    nav

--- components/navbarcomponents/Navbar.js
+++ components/navbarcomponents/Navbar.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import logo from "/public/logo.png";
+import SearchInput from "./SearchInput";
 
 const Navbar = () => {
   return (
@@ -8,6 +9,9 @@ const Navbar = () => {
         <div className="flex items-center">
           <Image src={logo} width={50} height={50} alt="logo" />
           <div className="text-3xl text-sky-300 font-bold pl-2">Barand</div>
+        </div>
+        <div>
+          <SearchInput />
         </div>
       </div>
     </div>
---
+++ components/navbarcomponents/SearchInput.js
@@ -0,0 +1,17 @@
+const SearchInput = () => {
+  return (
+    <div className="flex ">
+      <div>
+        <input />
+      </div>
+      <div>
+        <select>
+          <option>All category</option>
+        </select>
+      </div>
+      <button>Search</button>
+    </div>
+  );
+};
+
+export default SearchInput;
